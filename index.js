21a3ba5a502e33b1b248a744ad416a2ea5bb8bff
nadhub <nadir.hamaidi@gmail.com>
2014-10-12 17:53:02 +0000

    commit

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 
 'use strict';
 
-var express = require('express'),
-    app = express();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+
+
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+
 
 app.get('*', function(req, res){
     res.render('index');
 })
-
-app.listen(3000)
-console.log('Server running on port 3000 ...')
+var port = process.env.PORT || 3000;
+app.listen(port)
+console.log('Server running on ' + port + '...')
